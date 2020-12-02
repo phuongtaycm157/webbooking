087900f4c4dc5de7313d2dc4b0a8dac08e68b103
Nishi <ngudai999@gmail.com>
2020-12-02 08:22:31 +0700

    mini fix

--- routes/guest.js
+++ routes/guest.js
@@ -6,8 +6,11 @@ var con = require('../DB');
 
 router.get('/', function(req, res, next) {
   res.render('guest/index');
-})
+});
 
-
+router.post('/search', function(req, res, next) {
+	var findKeyWord = req.body.findKeyWord;
+	res.send(findKeyWord);
+});
 
 module.exports = router;
